90546fa6844b56911e58e7b915d254917137715f
RafalPenkala <raf.tricity@gmail.com>
2018-02-21 16:44:52 +0100

    testing Router-dom Link

--- src/components/PhotosView.js
+++ src/components/PhotosView.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
 
 class PhotosView extends Component {
   render() {
     return (
       <div>
-        <h1>Photos of some guys and girls.</h1>
+        <h1>
+          <Link to="/about">Photos of some guys and girls</Link>
+        </h1>
       </div>
     );
   }
