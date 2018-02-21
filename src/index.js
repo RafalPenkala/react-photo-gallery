aecbb6767901d037c37417abbc7523764ac64ae4
RafalPenkala <raf.tricity@gmail.com>
2018-02-21 15:06:50 +0100

    creating react router dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 import './index.css';
-import App from './App';
-
-
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render (
+  <Router>
+
+    
+  </Router>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
